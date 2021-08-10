367ee05b9ae3fff03ce8724366070d20e42861cf
J6idot <sesasoh@gmail.com>
2021-08-10 13:16:09 -0700

    removed bootanimation static hal

--- device.mk
+++ device.mk
@@ -30,23 +30,3 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6785
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6785 \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
