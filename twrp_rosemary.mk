fe68fee224953502c244673909c2d1e7e3602b05
J6idot <sesasoh@gmail.com>
2021-08-11 12:29:17 -0700

    added dynamic partition line

--- twrp_rosemary.mk
+++ twrp_rosemary.mk
@@ -16,6 +16,9 @@
 # limitations under the License.
 #
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
