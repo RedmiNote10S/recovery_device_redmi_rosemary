6af2edfc31041ea30431728c9a7838374586c81d
mastersenpai05 <pampangavergil0405@gmail.com>
2021-10-12 22:05:35 +0800

    rosemary: add needed in bootimage

--- twrp_rosemary.mk
+++ twrp_rosemary.mk
@@ -18,7 +18,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
