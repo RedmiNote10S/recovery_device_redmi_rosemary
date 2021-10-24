cd9fad0c4bd4546ea12ad9fd3a51885e1f5d4213
Woomy <woomy@woomy.be>
2021-10-24 20:20:54 +0200

    Device: Other tries for FBD

--- device.mk
+++ device.mk
@@ -41,6 +41,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # V A/B
 ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -69,7 +70,9 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Health Hal
 PRODUCT_PACKAGES += \
-     android.hardware.health@2.1-impl
+     android.hardware.health@2.1-impl \
+     android.hardware.health@2.1-service \
+     libhealthd.$(PRODUCT_PLATFORM)
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
