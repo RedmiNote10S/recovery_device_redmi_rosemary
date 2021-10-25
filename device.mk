eaabeb21471fb1b1042e503bb530118b5a8a9d3e
Woomymy <github@woomy.ovh>
2021-10-25 19:42:13 +0200

    Device: Other tries for FBD

--- device.mk
+++ device.mk
@@ -47,6 +47,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-recovery \
     android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
 PRODUCT_PACKAGES_DEBUG += \
@@ -73,8 +74,4 @@ PRODUCT_PACKAGES += \
      android.hardware.health@2.1-impl \
      android.hardware.health@2.1-service \
      libhealthd.$(PRODUCT_PLATFORM)
-
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
-
 
