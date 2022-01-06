e3f3ab2b2a31d0920909ad53fb79d575d467d12e
Woomymy <github@woomy.ovh>
2022-01-06 18:44:00 +0100

    VendorSetup: Fix indentation

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -42,8 +42,6 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]
 then
 	echo -e "\x1b[96mSetting up OrangeFox build vars...\x1b[m"
 	if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   		export TW_DEFAULT_LANGUAGE="en"
-		export LC_ALL="C"
 	 	export OF_FLASHLIGHT_ENABLE=0
 		export ALLOW_MISSING_DEPENDENCIES=true
 		export TARGET_DEVICE_ALT="maltose, secret"
@@ -54,14 +52,12 @@ then
 		export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		export OF_NO_MIUI_PATCH_WARNING=1
 		export FOX_USE_BASH_SHELL=1
 		export FOX_ASH_IS_BASH=1
 		export FOX_USE_TAR_BINARY=1
 		export FOX_USE_SED_BINARY=1
 		export FOX_USE_XZ_UTILS=1
-		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	    	export OF_USE_TWRP_SAR_DETECT=1
+	   	export OF_USE_TWRP_SAR_DETECT=1
 		export OF_NO_SAMSUNG_SPECIAL=1
 		export OF_QUICK_BACKUP_LIST="/boot;/data;"
 	    export FOX_DELETE_AROMAFM=1
@@ -69,9 +65,7 @@ then
 		export FOX_USE_NANO_EDITOR=1
 	    # OTA
 	    export OF_KEEP_DM_VERITY=1
-	    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 	    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 		export OF_USE_NEW_MAGISKBOOT=1
 		export OF_ENABLE_LPTOOLS=1
 		# screen settings
@@ -83,13 +77,10 @@ then
 		export OF_CLOCK_POS=1
 		# maximum permissible splash image size (in kilobytes); do *NOT* increase!
 		export OF_SPLASH_MAX_SIZE=104
-		export OF_PATCH_AVB20=1
 		export FOX_VERSION=R11.1_1
-		# run a process after formatting data to work-around MTP issues
 		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
 		export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
-		export OF_AB_DEVICE=1
-	export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+        export OF_AB_DEVICE=1
 		export FOX_USE_SPECIFIC_MAGISK_ZIP="$(gettop)/device/redmi/rosemary/Magisk/Magisk.zip"	
 	fi
 fi
