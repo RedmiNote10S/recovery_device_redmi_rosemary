e29acd192cccd78f45a88578281f1b55086ebb60
Woomy <woomy@woomy.be>
2022-06-21 17:52:55 +0200

    bootctrl: Update to version 1.2
    * Implement getActiveBootSlot

--- device.mk
+++ device.mk
@@ -61,8 +61,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-mtkimpl
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
