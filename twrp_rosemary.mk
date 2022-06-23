cdfd1947d7c2858a41c364331220c4f6efa4d435
Woomy <woomy@woomy.be>
2022-06-23 19:07:54 +0200

    twrp_rosemary: Update PRODUCT_MODEL

--- twrp_rosemary.mk
+++ twrp_rosemary.mk
@@ -33,6 +33,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 PRODUCT_DEVICE := rosemary
 PRODUCT_NAME := twrp_rosemary
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Note 10S
+PRODUCT_MODEL := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_RELEASE_NAME := Redmi Note 10S
